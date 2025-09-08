caaadb71b5b2facad12b46c6927787826f5a26bc
Programminghero1 <sajjadhossin351@gmail.com>
2025-09-08 16:35:28 +0600

    added a heart and love count

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// section-1
-let count = 0;
-const loveCount = document.getElementById("love-count");
-const loveIcons = document.getElementsByClassName("hardPng");
-for (const icon of loveIcons) {
-  icon.addEventListener('click', function () {
-    count++;
-    loveCount.textContent = count;
-  });
-}
+// heart and love 
+const hearts = document.querySelectorAll('.fa-heart');
+const loveCount = document.getElementById('love-count');
+
+hearts.forEach((heart) => {
+    heart.addEventListener('click', () => {
+        heart.classList.toggle('text-red-500');
+        heart.classList.toggle('fa-solid');
+        let count = parseInt(loveCount.textContent);
+        if (heart.classList.contains('fa-solid')) {
+            count++;
+        } else {
+            count--;
+        }
+        loveCount.textContent = count;
+    });
+});
